bffc4d37b93224f67f4fe940f968b8f8d90a8d87
Virginie Gautier <123579849+vgautier1@users.noreply.github.com>
2024-01-30 16:24:47 +0100

    Add files via upload

--- USE-CASE-1_EXR-correlation_partie2.R
+++ USE-CASE-1_EXR-correlation_partie2.R
@@ -13,3 +13,12 @@ calculate_pvalues <- function(cor_matrix) {
 }
 
 pvalues = round(calculate_pvalues(dataset),2)
+
+edge_colors = ifelse(cor_matrix > 0, "green", "red")
+edge_colors[pvalues > 0.05 ] = "white"  # Liens de corrélations blanc pour les corrélations NS (seuil 5%)
+
+qgraph(cor_matrix,
+       labels = colnames(cor_matrix), # Label dans les bulles
+       layout = "spring",             # Sortie sous forme de réseau
+       edge.color = edge_colors       # Intégre la condition sur les liens NS
+)
